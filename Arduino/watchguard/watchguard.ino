645143667318d218e8ca578109d65a310f19f334
chrizmoko <crmok@uci.edu>
2022-02-18 10:38:07 -0800

    Updates...

--- Arduino/watchguard/watchguard.ino
+++ Arduino/watchguard/watchguard.ino
@@ -1,7 +1,8 @@
+#define USE_ARDUINO_INTERRUPTS true
+
+#include <SoftwareSerial.h>
 #include <PulseSensorPlayground.h>
 #include <LiquidCrystal_I2C.h>
-
-#include "led.h"
 
 // LCD variables
 const uint8_t LCD_ID = 0x27;
@@ -10,40 +11,86 @@ const uint8_t LCD_COLUMNS = 16;
 LiquidCrystal_I2C lcd(LCD_ID, LCD_COLUMNS, LCD_ROWS);
 
 // Pulse sensor variables
-const uint8_t PULSE_SENSOR_ANALOG_PIN = 4;
+const int PULSE_SENSOR_ANALOG_PIN = A4;
+const int PULSE_SENSOR_LED_PIN = 5;
 int sensor_data;
 PulseSensorPlayground pulse_sensor;
+int print_bpm;
 
+// Methane sensor
+const int GAS_SENSOR_ANALOG_PIN = A0;
+int print_gas;
 
-const char* messages[] = {"Hello world!", "Today is nice.", "It's okay."};
+// RF Module
+SoftwareSerial mySerial(20, 21);
 
+// String
+void lcd_print(int bpm, int gas);
+int slow_update = 0;
 
 void setup() {
-    Serial.begin(9600);
+    const int BAUD_RATE = 9600;
+    
+    Serial.begin(BAUD_RATE);
 
+    mySerial.begin(BAUD_RATE);
+
+    // Setup pulse sensor
     pulse_sensor.analogInput(PULSE_SENSOR_ANALOG_PIN);
-    pulse_sensor.blinkOnPulse(4);
+    pulse_sensor.blinkOnPulse(PULSE_SENSOR_LED_PIN);   // blink LED
+    pulse_sensor.setThreshold(550); // prevents noise
     pulse_sensor.begin();
     
+    // Setup lcd
     lcd.begin();
-    
-    // Turn on the backlight
     lcd.backlight();
-    
-    // Clear and print
     lcd.clear();
-    lcd.print("Hello world!");
+    lcd.setCursor(0, 0);
+    lcd.print("bpm: ?");
+    lcd.setCursor(0, 1);
+    lcd.print("gas level: ?");
 }
 
 
 void loop() {
     int bpm = pulse_sensor.getBeatsPerMinute();
-
+    int gas = analogRead(GAS_SENSOR_ANALOG_PIN);
+    
     if (pulse_sensor.sawStartOfBeat())
     {
-        lcd.clear();
-        lcd.print(String(bpm));
+        print_bpm = bpm;
+        lcd_print(print_bpm, print_gas);
     }
 
+    print_gas = gas;
+    if (slow_update == 200) {
+        lcd_print(print_bpm, print_gas);
+        slow_update = 0;
+    }
+
+    if (mySerial.available()) {
+        lcd.setCursor(0, 1);
+        lcd.print("received: " + String(mySerial.read()));
+    }
+
+    slow_update++;
+    
+    // Loop pause
     delay(20);
+}
+
+
+void lcd_print(int bpm, int gas)
+{   
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("bpm: " + String(bpm) + "    ");
+    lcd.setCursor(0, 1);
+    if (mySerial.available()) {
+        lcd.print(char(mySerial.read()));
+    } else {
+        lcd.print("gas level: " + String(gas) + "   ");
+    }
+
+    mySerial.write(bpm);
 }
